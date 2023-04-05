49d8bcc8a0252d74ef5f6f5576a1593833f87e61
gibeongideon <kipngeno.gibeon@gmail.com>
2023-04-06 00:55:42 +0300

    add updated sots +docs

--- lib/utils/constants/api_consts.dart
+++ lib/utils/constants/api_consts.dart
@@ -2,4 +2,4 @@ String BASE_URL = "https://api.openai.com/v1";
 String baseUrl = "https://api.openai.com/v1/completions";
 String apiUrl = "https://api.openai.com/v1/images/generations";
 
-String API_KEY = "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";
+String API_KEY = "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";
