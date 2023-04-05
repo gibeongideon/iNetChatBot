a326006b638e18d5a854a85bc603bcd2722a1827
gibeongideon <kipngeno.gibeon@gmail.com>
2023-04-05 19:09:44 +0300

    change OpenAI key

--- lib/utils/constants/api_consts.dart
+++ lib/utils/constants/api_consts.dart
@@ -1,4 +1,5 @@
 String BASE_URL = "https://api.openai.com/v1";
 String baseUrl = "https://api.openai.com/v1/completions";
 String apiUrl = "https://api.openai.com/v1/images/generations";
+
 String API_KEY = "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";
